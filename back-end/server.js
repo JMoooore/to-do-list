94318bdc3d11e647604dc33d9376060302583220
John Moore <johnhtmoore@gmail.com>
2022-01-28 14:22:53 -0700

    added routes for new task, update task, get single task, sorting

--- back-end/server.js
+++ back-end/server.js
@@ -26,6 +26,76 @@ app.get('/todos', async (req,res) => {
     }
 })
 
+app.get('/todos/:id', async (req,res) => {
+    try {
+        const id = req.params.id
+        const {rows} = await pool.query("SELECT * FROM tasks WHERE id = $1", [id])
+        res.send(rows)
+    } catch (error) {
+        errorHandling(error, res)
+    }
+})
+
+app.get('/todos?sort=user', async (req,res) => {
+    try {
+        const {rows} = await pool.query("SELECT * FROM tasks ORDER BY task_owner")
+        res.send(rows)
+    } catch (error) {
+        errorHandling(error, res)
+    }
+})
+
+app.get('/todos?sort=importance', async (req,res) => {
+    try {
+        const {rows} = await pool.query("SELECT * FROM tasks ORDER BY task_importance")
+        res.send(rows)
+    } catch (error) {
+        errorHandling(error, res)
+    }
+})
+
+
+app.post('/todos', async (req,res) => {
+    try {
+        const {importance, title, owner, complete} = req.body
+        await pool.query(`INSERT INTO tasks(task_importance, task_title, task_owner, task_complete) VALUES ($1, $2, $3, $4)`,
+            [importance, title, owner, complete]
+        );
+        res.send('updated');
+    } catch (err) {
+        res.status(500);
+        res.json(err);
+    }
+})
+
+app.patch('/todos/:id', async (req,res) => {
+    try {
+        const id = req.params.id
+        const dbData = await pool.query(`SELECT * FROM tasks WHERE id = $1`, [id])
+        const singleTask = dbData.rows[0];
+        let {task_importance, task_title, task_owner, task_complete} = singleTask;
+        if (req.body.importance) {
+            task_importance = req.body.importance
+        }
+        if (req.body.title)  {
+            task_title = req.body.title
+        }
+        if (req.body.owner) {
+            task_owner = req.body.owner
+        }
+        if (req.body.complete) {
+            task_complete = req.body.complete
+        }
+        await pool.query(`UPDATE tasks SET task_importance = $1, task_title = $2, task_owner = $3, task_complete = $4 WHERE id = $5`,
+            [task_importance, task_title, task_owner, task_complete, id]
+        );
+        res.send('updated');
+    } catch (error) {
+        res.status(500);
+        res.json(error);
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Listening on Port: ${PORT}`);
 })
